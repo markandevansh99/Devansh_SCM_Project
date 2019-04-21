cb7545f710096dcbf8a46aee9ebe04d7b7891b9b
markandevansh99 <500069794@stu.upes.ac.in>
2019-04-21 12:36:33 +0530

    Singly linked list with 3 operations(create, insert,delete node)

--- Implementation.cpp
+++ Implementation.cpp
@@ -95,3 +95,155 @@ return 0;
 
 }
 
+
+
+//Now, I am Implementing Singly Linked List as One of the operations in MAIN MENU which includes Create(),Insert(),Delete()
+
+
+//SINGLY LINKED LIST
+void singly()
+{
+class Node1
+{
+public:
+    int info;
+    Node* next;
+};
+class List:public Node1
+{
+
+    Node *first,*last;
+public:
+    List()
+    {
+        first=NULL;
+        last=NULL;
+    }
+    void create();
+    void insert();
+    void delet();
+    void display();
+    void search();
+};
+void List::create()
+{
+    Node1 *temp;
+    temp=new Node1;
+    int n;
+    cout<<"\nEnter an Element:";
+    cin>>n;
+    temp->info=n;
+    temp->next=NULL;
+    if(first==NULL)
+    {
+        first=temp;
+        last=first;
+    }
+
+    else
+    {
+        last->next=temp;
+        last=temp;
+    }
+}
+void List::insert()
+{
+    Node *prev,*cur;
+    prev=NULL;
+    cur=first;
+    int count=1,pos,ch,n;
+    Node *temp=new Node;
+    cout<<"\nEnter an Element:";
+    cin>>n;
+    temp->info=n;
+    temp->next=NULL;
+    cout<<"\nINSERT AS\n1:FIRSTNODE\n2:LASTNODE\n3:IN BETWEEN FIRST&LAST NODES";
+    cout<<"\nEnter Your Choice:";
+    cin>>ch;
+    switch(ch)
+    {
+    case 1:
+        temp->next=first;
+        first=temp;
+        break;
+    case 2:
+        last->next=temp;
+        last=temp;
+        break;
+    case 3:
+        cout<<"\nEnter the Position to Insert:";
+        cin>>pos;
+        while(count!=pos)
+        {
+            prev=cur;
+            cur=cur->next;
+            count++;
+        }
+        if(count==pos)
+        {
+            prev->next=temp;
+            temp->next=cur;
+        }
+        else
+            cout<<"\nNot Able to Insert";
+        break;
+
+    }
+}
+void List::delet()
+{
+    Node *prev=NULL,*cur=first;
+    int count=1,pos,ch;
+    cout<<"\nDELETE\n1:FIRSTNODE\n2:LASTNODE\n3:IN BETWEEN FIRST&LAST NODES";
+    cout<<"\nEnter Your Choice:";
+    cin>>ch;
+    switch(ch)
+    {
+    case 1:
+        if(first!=NULL)
+        {
+            cout<<"\nDeleted Element is "<<first->info;
+            first=first->next;
+        }
+        else
+            cout<<"\nNot Able to Delete";
+        break;
+    case 2:
+        while(cur!=last)
+        {
+            prev=cur;
+            cur=cur->next;
+        }
+        if(cur==last)
+        {
+            cout<<"\nDeleted Element is: "<<cur->info;
+            prev->next=NULL;
+            last=prev;
+        }
+        else
+            cout<<"\nNot Able to Delete";
+        break;
+    case 3:
+        cout<<"\nEnter the Position of Deletion:";
+        cin>>pos;
+        while(count!=pos)
+        {
+            prev=cur;
+            cur=cur->next;
+            count++;
+        }
+        if(count==pos)
+        {
+            cout<<"\nDeleted Element is: "<<cur->info;
+            prev->next=cur->next;
+        }
+        else
+            cout<<"\nNot Able to Delete";
+        break;
+    }
+}
+
+
+
+
+
